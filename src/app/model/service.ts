771546ffbfd3b81471ebdca6e3cba5e3a9d5ffc0
Hubert MOKET <hubertayiwa@gmail.com>
2020-02-17 10:13:40 +0100

    class service

---
+++ src/app/model/service.ts
@@ -0,0 +1,36 @@
+import {Metier} from './metier';
+
+export class Service {
+    constructor(
+    private _idService: number,
+    private _nomService: string,
+    private _metier: Metier
+    )
+{
+    }
+
+
+    get idService(): number {
+        return this._idService;
+    }
+
+    set idService(value: number) {
+        this._idService = value;
+    }
+
+    get nomService(): string {
+        return this._nomService;
+    }
+
+    set nomService(value: string) {
+        this._nomService = value;
+    }
+
+    get metier(): Metier {
+        return this._metier;
+    }
+
+    set metier(value: Metier) {
+        this._metier = value;
+    }
+}
