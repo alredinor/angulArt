82d8cc17f21f9da4e20f9ff47239c803b2168e90
Hubert MOKET <hubertayiwa@gmail.com>
2020-02-17 12:17:09 +0100

    hubert

--- src/app/components/service-java/service-java.component.ts
+++ src/app/components/service-java/service-java.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {ServiceJava} from '../../model/service-java';
+import {ServiceJavaService} from '../../../services/service-java-service';
 
 @Component({
   selector: 'app-service-java',
@@ -9,9 +10,27 @@ import {ServiceJava} from '../../model/service-java';
 export class ServiceJavaComponent implements OnInit {
 
   private services: ServiceJava [] = [];
-  constructor() { }
+  constructor(private serviceJavaService: ServiceJavaService) { }
 
   ngOnInit() {
+    this.list();
+  }
+
+  // liste des services
+  private list(){
+    this.serviceJavaService.findAll().subscribe(result => {
+      console.log(result);
+      this.services = result;
+    }, error1 => {
+      console.log('pas bien');
+    });
+  }
+
+  // suppression du service par son nom
+  public delete(nomService: string) {
+    this.serviceJavaService.delete(nomService).subscribe(result => {
+      this.list();
+    });
   }
 
 }
