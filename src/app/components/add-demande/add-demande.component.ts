1aaa2f00bf7d258c690048e20347575f78400825
Nico <touboulnicolas@gmail.com>
2020-02-17 11:44:55 +0100

    nico

--- src/app/components/add-demande/add-demande.component.ts
+++ src/app/components/add-demande/add-demande.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {Demande} from '../../model/demande';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-add-demande',
@@ -7,7 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddDemandeComponent implements OnInit {
 
-  constructor() { }
+  private demande: Demande = new Demande('100');
+
+  constructor(private router: Router, private demandeService) { }
+
+  public save(){
+    this.demandeService.insert(this)
+  }
 
   ngOnInit() {
   }
