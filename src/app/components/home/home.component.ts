3ed40be10111961f782d8fe4fb4f3d6c7a29c670
Hubert MOKET <hubertayiwa@gmail.com>
2020-02-18 15:36:15 +0100

    home hubert

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private image = '../../../assets/images/artisan/imageArt.jpg';
   constructor() { }
 
   ngOnInit() {
