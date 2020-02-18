c2a062bd48e19679b6eb0817100cdb69fae9b7d5
Nico <touboulnicolas@gmail.com>
2020-02-18 10:29:51 +0100

    nico

--- src/app/edit-demande/edit-demande.component.ts
+++ src/app/edit-demande/edit-demande.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {DemandeService} from '../../services/demande.service';
+import {Demande} from '../model/demande';
 
 @Component({
   selector: 'app-edit-demande',
@@ -7,7 +10,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditDemandeComponent implements OnInit {
 
-  constructor() { }
+  private demande: Demande = new Demande();
+  private create = true;
+  private demandeOld: Demande = null;
+  private index: number;
+
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private demandeService: DemandeService) {
+    this.activatedRoute.params.subscribe(params => {
+      if (params.index) {
+        this.index = params.index;
+        this.create = false;
+        this.demande = this.demandeService.demandes[params.index];
+        this.demandeOld = new Demande (this.demande.idDemande, this.demande.message, this.demande.artisan, this.demande.client,
+            this.demande.service, this.demande.metier, this.demande.statut, this.demande.date);
+      }
+    });
+  }
+  public save() {
+    this.demandeService.insert(this.demande).subscribe(resutlt => {
+      this.router.navigate(['/demande']);
+    });
+  }
 
   ngOnInit() {
   }
