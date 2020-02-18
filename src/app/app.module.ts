a45e3bb4c43708bf8de5092c77a8f5149069d3d8
Nico <touboulnicolas@gmail.com>
2020-02-18 12:28:49 +0100

    nico

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,7 @@ import { AddMetierComponent } from './components/add-metier/add-metier.component
 import { AddServiceJavaComponent } from './components/add-service-java/add-service-java.component';
 import { EditServiceJavaComponent } from './components/edit-service-java/edit-service-java.component';
 import {AddCompteComponent} from './components/add-compte/add-compte.component';
-import { EditDemandeComponent } from './edit-demande/edit-demande.component';
+import { EditDemandeComponent } from './components/edit-demande/edit-demande.component';
 import { AddArtisanComponent } from './components/add-artisan/add-artisan.component';
 import { AddClientComponent } from './components/add-client/add-client.component';
 
