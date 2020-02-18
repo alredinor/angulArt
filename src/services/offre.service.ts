877213a7955a9df96ad8252fb218710ab5af16af
Nico <touboulnicolas@gmail.com>
2020-02-18 17:16:31 +0100

    nico

---
+++ src/services/offre.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import {Demande} from '../app/model/demande';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class OffreService {
+
+  private url = 'http://localhost:8080/art/rest/offre'
+  private options: object;
+  constructor(private http: HttpClient) {
+  }
+
+  private authentification(){
+    this.headers = new HttpHeaders({ 'Content-Type': 'application/json',
+      'Authorization': 'Basic ' + sessionStorage.getItem('user')});
+    this.options = {headers: this.headers};
+  }
+  public findAll(): Observable<any> {
+    this.authentification()
+    return this.http.get(this.url, this.options);
+  }
+
+  public delete(id: number): Observable<any>{
+
+    return this.http.delete(this.url + '/' + id, this.options) ;
+  }
+  public insert(offre: Offre): Observable<any> {
+    this.authentification()
+    const  o: object = {
+      service: offre.service,
+      metier: offre.metier,
+      artisan: offre.artisan
+    };
+    return this.http.post(this.url + '/addOffre', o, this.options);
+  }
+}
