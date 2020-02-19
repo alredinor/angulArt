008d2a053edd8ad349f68c504fa9b0db74d36e20
alban <alban.delouvee@gmail.com>
2020-02-19 15:50:39 +0100

    alban

--- src/app/components/edit-compte/edit-compte.component.ts
+++ src/app/components/edit-compte/edit-compte.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {Compte} from '../../model/compte';
 import {CompteService} from '../../../services/compte.service';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-edit-compte',
@@ -10,9 +11,14 @@ import {CompteService} from '../../../services/compte.service';
 export class EditCompteComponent implements OnInit {
 
   private compte: Compte;
+  constructor(private router: Router, private activatedRoute: ActivatedRoute, private compteService: CompteService) {
+  }
 
-  constructor(private compteService: CompteService) {
+  public save() {
 
+    this.compteService.edit(this.compte, this.compte.idCompte).subscribe(resutlt => {
+      this.router.navigate(['/compte']);
+    });
   }
 
   ngOnInit() {}
