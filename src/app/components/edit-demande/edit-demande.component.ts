117277f06892dc9948f01416409056b233503463
Nico <touboulnicolas@gmail.com>
2020-02-20 11:51:11 +0100

    nicolas

--- src/app/components/edit-demande/edit-demande.component.ts
+++ src/app/components/edit-demande/edit-demande.component.ts
@@ -1,9 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute, convertToParamMap, Router} from '@angular/router';
 import {DemandeService} from '../../../services/demande.service';
 import {Demande} from '../../model/demande';
-import {StatutDemande} from '../../statut-demande.enum';
-import {EnumValue} from '@angular/compiler-cli/src/ngtsc/partial_evaluator';
 
 @Component({
   selector: 'app-edit-demande',
@@ -14,14 +12,15 @@ export class EditDemandeComponent implements OnInit {
 
   private demande: Demande = new Demande();
   private demandeOld: Demande = null;
+  private index: number;
 
 
   constructor(private router: Router, private activatedRoute: ActivatedRoute, private demandeService: DemandeService) {
 
-    this.activatedRoute.params.subscribe(param => {
-      if (param.idDemande) {
-        this.demande.idDemande = param.idDemande;
-        this.demande = this.demandeService.findAll()[param.idDemande];
+    this.activatedRoute.params.subscribe(params => {
+      if (params.demande.idDemande) {
+        this.demande.idDemande = params.demande.idDemande;
+        this.demande = this.demandeService.findAll[params.demande.idDemande];
         this.demandeOld = new Demande (this.demande.idDemande, this.demande.message,
             this.demande.client, this.demande.statut, this.demande.date, this.demande.offre);
       }
@@ -36,6 +35,13 @@ export class EditDemandeComponent implements OnInit {
     //   this.demandeService.edit(this.demande, param.params.idDemande).subscribe(resutlt => {this.router.navigate(['/demande']);
     //   });
     //  });
+  }
+
+  public cancel() {
+
+      this.demandeService._demandes[this.demande.idDemande] = this.demandeOld;
+
+      this.router.navigate(['/demande']);
   }
   ngOnInit() {
   }
